69c545ad6e3bfda67e4f53ee224655ae59a1dc32
bharti3bk <bharti3bk@gmail.com>
2019-09-12 11:41:42 -0700

    updated functions

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -11,37 +11,64 @@
  }
 
 function find() {
- return db('schemes')
+ return db('schemes') 
+  .then(schemes => {
+      return schemes;
+  })
 } 
 
 function findById(id) {
   return db("schemes")
-         .where({id})
-         .first();
+         .where({id : id})
+         .then(scheme => {
+             if(scheme){
+                 return scheme[0];
+             }
+             else {
+                 return null;
+             }
+         })
 }  
 
-function remove(id){
-    return db('schemes')
-    .where({id})
-    .del();
- }
-function findSteps(id){
-  return db.select("steps.id" , "schemes.scheme_name" , "steps.step_number" , "steps.instructions"
-  ).from("steps")
-  .innerJoin("schemes" , "steps.scheme_id" , "=" , "schemes.id")
-  .where("steps.scheme_id" , "=" , id)
+function remove(id){ 
+    const removed = findById(id);
+    return db("schemes")
+    .where({id : id})
+    .del()
+    .then(scheme => {
+        if(scheme) {
+            return removed;
+        } 
+        else {
+            return null;
+        }
+    });
+ } 
+
+function findSteps(id){ 
+ return db("schemes")
+ .join("steps" , "schemes.id" , "steps.scheme_id") 
+ .where({ scheme_id : id })
+ .select(
+     "steps.id" , "schemes.scheme_name" , "steps.step_number" ,"steps.instructions"
+ )
+ .orderBy("schemes.id")
+ .then(steps => {
+     return steps;
+ })
+  
 } 
 
 function add(scheme) {  
   return db('scheme')
    .insert(scheme)  
    .then(id => {
-       return findById(id);
+       return findById(id[0]);
    });
 }
 
 function update(schemeChanges , id) {
-  return db('schemeChanges')
+  return db('schemes')
   .where({id: id})
   .update(schemeChanges)
   .then(schemeChanges => {
